826eaf1a3f472b035efe52538e319b0f3c2ea6f4
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-24 22:20:19 +0200

    get data from API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class AlbumList extends React.Component {
+class AlbumList extends Component {
+//executed intantly after render
+  componentWillMount() {
+    // ASYNC HTTP Request to get albums from the API.
+    // eslint-disable-next-line
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseData) => console.log(responseData));
+  }
     render() {
-      return (<View>
-    <Text> Album List component</Text>
-    </View>);
+      return (
+        <View>
+          <Text> Album List component</Text>
+        </View>
+      );
   }
-  }
-
+}
 
 export default AlbumList;
