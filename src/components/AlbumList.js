323b2bca45f3b5828de27d2fa89acda7df4132f3
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-24 21:43:39 +0200

    use class component instead of functional component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-class AlbumList extends  (
-    <View>
-    <Text> Album List </Text>
-    </View>
-);
+class AlbumList extends React.Component {
+    render() {
+      return (<View>
+    <Text> Album List component</Text>
+    </View>);
+  }
+  }
 
 
 export default AlbumList;
