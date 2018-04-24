19e9786b36206b347d3aea454889882da6f9723e
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-24 20:20:49 +0200

    simple app with some text

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
+//create a component
+const App = () => (
+    //JSX! it's JS syntactic sugar, mask over normal function calls
+  <Text>Some text!</Text>
+);
+
+//rendering
 AppRegistry.registerComponent('albums', () => App);
