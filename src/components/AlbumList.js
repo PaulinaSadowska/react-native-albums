55b4fa5cf2fe2d5eadc5fbf32c4e9f703c9ce832
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-26 22:12:08 +0200

    create card and card section to style items fetched from API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+
+state = { albums: [] };
+
 //executed intantly after render
   componentWillMount() {
     // ASYNC HTTP Request to get albums from the API.
     // eslint-disable-next-line
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then((response) => response.json())
-    .then((responseData) => console.log(responseData));
+    .then((responseData) => this.setState({ albums: responseData }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
     render() {
+      console.log(this.state);
       return (
         <View>
-          <Text> Album List component</Text>
+          { this.renderAlbums() }
         </View>
       );
   }
