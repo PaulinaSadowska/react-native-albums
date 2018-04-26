85fcb5d95bfef969cb6eecaefa6944b525099f1f
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-26 23:34:37 +0200

    show album cover and style header

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, AppRegistry } from 'react-native';
 import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList'
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <View>
+    <View styles={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
     </View>
